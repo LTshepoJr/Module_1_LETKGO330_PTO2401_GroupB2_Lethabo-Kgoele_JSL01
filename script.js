c6191dd29a38f20ff715791a0d1bf93fbd4b5b33
Lethabo Kgoele <lethabotshepojr@gmail.com>
2024-05-16 20:36:23 +0200

    validation done

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 function validateSyntax() {
-    let input = document.getElementById('petInput').value;
-    // Validation logic goes here
-    let result = ''; // Placeholder for validation result
+  let input = document.getElementById("petInput").value;
+  let regex = /^pet_[a-zA-Z0-9]+$/; // Validation logic goes here
+  let result = ""; // Placeholder for validation result
 
-    // TODO: Write your validation logic here
-        // Check if input starts with 'pet_' and followed by alphanumeric characters
+  // TODO: Write your validation logic here
+  // Check if input starts with 'pet_' and followed by alphanumeric characters
 
-            document.getElementById('result').innerText = result;
+  if (regex.test(input)) {
+    result = "Valid Syntax ✅";
+  } else {
+    result = "Invalid Syntax ❌";
+  }
+  document.getElementById("result").innerText = result;
 }
-
-
